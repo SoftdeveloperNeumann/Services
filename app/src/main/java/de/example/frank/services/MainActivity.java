2c747c4b21ef71ba6fc17b20b3c008e01e14c668
Administrator <f.neumann.berlin@googlemail.com>
2016-09-30 10:13:20 +0200

    Initial commit

---
+++ app/src/main/java/de/example/frank/services/MainActivity.java
@@ -0,0 +1,48 @@
+package de.example.frank.services;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{
+                            Manifest.permission.READ_CALL_LOG},
+                    2);
+
+        }
+    }
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults){
+        switch (requestCode) {
+            case 2: {
+                if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(this,DemoService.class);
+                    startService(intent);
+                } else {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        shouldShowRequestPermissionRationale(permissions[0]);
+                    }
+                    Toast.makeText(this,
+                            "Permission fehler",
+                            Toast.LENGTH_LONG).show();
+//                    cancelOperation();
+                }
+                return;
+            }
+
+        }
+
+    }
+}
